eb02e5092e3c912474ba14b71ace262b00be7a94
xiaozongkang <wb_xiaozongkang@ocj.com.cn>
2017-07-08 14:01:32 +0800

    练习

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,12 +1,35 @@
-import 'react-native';
-import React from 'react';
-import Index from '../index.android.js';
+// import 'react-native';
+// import React from 'react';
+// import Index from '../index.android.js';
+//
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
+//
+// it('renders correctly', () => {
+//     const tree = renderer.create(
+//         <Index />
+//     );
+// });
+// class HelloWorldApp extends Component {
+//     render() {
+//         return (
+//             <Text>Hello world!</Text>
+//         );
+//     }
+// }
+//
+// // 注意，这里用引号括起来的'HelloWorldApp'必须和你init创建的项目名一致
+// AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp);
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+class HelloWorldApp extends Comment {
+    render() {
+        return (
+            <Text>Hello world!</Text>
+        );
+    }
+}
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
-});
+// 注意，这里用引号括起来的'HelloWorldApp'必须和你init创建的项目名一致
+AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp);
--- index.android.js
+++ index.android.js
@@ -4,50 +4,65 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image
 } from 'react-native';
 
+class Greeting extends Component {
+    render() {
+        return (
+            <Text>Hello{this.props.name}</Text>
+        );
+    }
+}
+
 export default class AwesomeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        let pic = {
+            uri: "https://www.baidu.com/img/bd_logo1.png"
+        };
+
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    环境光临RN
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.android.js
+                </Text>
+                <Text style={styles.instructions}>
+                    Double tap R on your keyboard to reload,{'\n'}
+                    Shake or press menu button for dev menu
+                </Text>
+                <Image source={pic} style={{width: 100, height: 80}}/>
+                <Greeting name="呵呵呵呵呵呵"/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
